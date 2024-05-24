b70cc7c5270e0940a6f7d14f3a74d377e92c73eb
Dhruv <dhruvanandsingh2004@gmail.com>
2024-05-25 00:59:33 +0530

    trying to solve a problem

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,5 +1,5 @@
 const User = require("../model/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 //register validation
 module.exports.register = async (req, res, next) => {
